317c13f40d1a309be90ece51a588d0e2cce91a2e
saleh04 <saleh04mo@gmail.com>
2026-09-02 19:18:10 +0300

    Refactor lifespan management in main.py; update app state for MongoDB, LLM, and VectorDB clients; include NLP routes

--- src/main.py
+++ src/main.py
@@ -1,35 +1,54 @@
 from contextlib import asynccontextmanager
 
-from async_pymongo import AsyncClient # type: ignore
+from async_pymongo import AsyncClient  # type: ignore
 from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
 
 from helpers.config import Settings
-from routes import base, data
+from routes import base, data, nlp
 from stores.llm.LLMProviderFactory import LLMProviderFactory
+from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     settings = Settings() # type: ignore
 
-    app.mongo_conn = AsyncClient(settings.MONGODB_URI) # type: ignore
-    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE] # type: ignore
+    #MongoDB connection
+    app.state.mongo_conn = AsyncClient(settings.MONGODB_URI) 
+    app.state.db_client = app.state.mongo_conn[settings.MONGODB_DATABASE] 
 
+    # LLM and VectorDB clients
     llm_provider_factory = LLMProviderFactory(settings)
+    vectordb_provider_factory = VectorDBProviderFactory(settings)
 
-    app.generation_client = llm_provider_factory.create(settings.GENERATION_BACKEND) # type: ignore
-    app.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID) 
+    # Generation
+    app.state.generation_client = llm_provider_factory.create(settings.GENERATION_BACKEND) 
+    app.state.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID) 
 
-    app.embedding_client = llm_provider_factory.create(settings.EMBEDDING_BACKEND) # type: ignore
-    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
+    # Embedding
+    app.state.embedding_client = llm_provider_factory.create(settings.EMBEDDING_BACKEND) # type: ignore
+    app.state.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
                                               embedding_size=settings.EMBEDDING_MODEL_SIZE)
 
+    # VectorDB
+    app.state.vectordb_client = vectordb_provider_factory.create(settings.VECTOR_DB_BACKEND)
+    app.state.vectordb_client.connect()
+    
     yield
 
-    app.mongo_conn.close()
+    app.state.mongo_conn.close()
+    app.state.vectordb_client.disconnect()
 
 app = FastAPI(lifespan=lifespan)
 
+@app.get("/scalar", include_in_schema=False)
+def scalar_docs():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="Scalar API"
+    )
                   
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+app.include_router(nlp.nlp_router)
