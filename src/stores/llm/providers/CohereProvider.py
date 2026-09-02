fb0d3547df5474e21e4f305e0903f4d7c7e8ea84
saleh04 <saleh04mo@gmail.com>
2026-09-02 19:16:58 +0300

    Fix CohereProvider class definition and enhance embedding methods; add support for batch text embedding

--- src/stores/llm/providers/CohereProvider.py
+++ src/stores/llm/providers/CohereProvider.py
@@ -6,14 +6,15 @@ from ..LLMEnums import CohereEnums, DocumentTypeEnum
 from ..LLMInterface import LLMInterface
 
 
-class ChohereProvider(LLMInterface):
+class CohereProvider(LLMInterface):
 
-    def __init__(self, api_key: str,
+    def __init__(self, api_key: str, api_url: str | None = None,
                      default_input_max_char: int = 1000,
                      default_generation_output_tokens: int = 1000,
                      default_generation_temp: float = 0.1):
     
         self.api_key = api_key
+        self.api_url = api_url
         self.default_input_max_char = default_input_max_char
         self.default_generation_output_tokens = default_generation_output_tokens
         self.default_generation_temp = default_generation_temp
@@ -22,7 +23,10 @@ class ChohereProvider(LLMInterface):
         self.embedding_model_id: str | None = None
         self.embedding_size: int | None = None
 
-        self.co = cohere.ClientV2(api_key=self.api_key)
+        if self.api_url:
+            self.co = cohere.ClientV2(api_key=self.api_key, base_url=self.api_url)
+        else:
+            self.co = cohere.ClientV2(api_key=self.api_key)
 
         self.logger = logging.getLogger(__name__)
 
@@ -53,9 +57,10 @@ class ChohereProvider(LLMInterface):
         if chat_history is None:
             chat_history= []
 
-        chat_history.append(
+        chat_history = [
             self.construct_prompt(role=CohereEnums.USER.value , prompt=prompt)
-        )
+        ] + chat_history
+
 
         response = self.co.chat(
             model = self.generation_model_id,
@@ -85,18 +90,40 @@ class ChohereProvider(LLMInterface):
             input_type = CohereEnums.QUERY.value
 
         response = self.co.embed(
-            input = [self.process_text(text)],
+            texts = [self.process_text(text)],
             model = self.embedding_model_id,
             input_type = input_type,
             embedding_types = ["float"]
-
         )
 
-        if not response or not response.embeddings or len(response.embeddings) == 0:
+        if not response or not response.embeddings or not response.embeddings.float:
             self.logger.error("Error while embedding text with Cohere")
             return None
 
-        return response.embedding.float[0]
+        return response.embeddings.float[0]
+
+    def embed_batch_texts(self, texts: list[str], document_type: str | None = None):
+
+        if not self.co or not self.embedding_model_id:
+            self.logger.error("Cohere Client or Model not set")
+            return None
+
+        input_type = CohereEnums.DOCUMENT.value
+        if document_type == DocumentTypeEnum.QUERY.value:
+            input_type = CohereEnums.QUERY.value
+
+        response = self.co.embed(
+            texts = [self.process_text(text) for text in texts],
+            model = self.embedding_model_id,
+            input_type = input_type,
+            embedding_types = ["float"]
+        )
+
+        if not response or not response.embeddings or not response.embeddings.float:
+            self.logger.error("Error while embedding batch texts with Cohere")
+            return None
+
+        return response.embeddings.float
 
     def construct_prompt(self, prompt:str, role:str):
         return {
